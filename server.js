fdf2b710f7cd316bdcee0bed3ad23562fd5c8917
tabishshah139 <54994056+tabishshah139@users.noreply.github.com>
2019-11-22 22:28:34 +0500

    Update server.js

--- server.js
+++ server.js
@@ -58,25 +58,25 @@ message.author.send("https://discordapp.com/api/oauth2/authorize?client_id=61992
 
 bot.on('message', message =>{
     if (!message.guild) return;
-if (message.content === '+spotify'){
+if (message.content === '+gen'){
     if (usedCommandRecently4.has(message.author.id)){
         message.channel.send('``Wait 2 mintue before generating again``')
     } else{
         usedCommandRecently4.add(message.author.id);
         setTimeout(() =>{
             usedCommandRecently4.delete(message.author.id);
-        }, 120000)
-    var string = `Note: We have added Little Advertisement. You Can Get more than 50 Accounts by seeing advertisement 1 time. Watch Advertisement and Recieve Alot Accounts Thank You. :one: ( https://up-to-down.net/41622/spotify1 ) 200x Spotify Accounts list 1. :two: ( https://up-to-down.net/41622/Spotify2P ) 200x Spotify Accounts list 2. :three: ( https://up-to-down.net/41622/spotify3 ) 200x Spotify Accounts list 3. :four: ( https://up-to-down.net/41622/spotify4 ) 128x Spotify Accounts list 4`;
+        }, 1000)
+    var string = `Note: We have added Little Advertisement. You Can Get more than 50 Accounts by seeing advertisement 1 time. :one: Mail Access ( https://link-to.net/41622/mailaccc4 ) 150x Mail Access Accounts list 1.  :two: Minecraft Accounts ( https://up-to-down.net/41622/minets3 ) 149x Minecraft Accounts list 1. :three: Hulu Accounts ( https://link-to.net/41622/hulu11 ) 1000x Hulu Accounts list 1. :four: Netflix Accounts ( https://link-to.net/41622/netflixe ) 200x Netflix Accounts list 1. :five: Spotify Accounts ( https://up-to-down.net/41622/spotify3 ) 200x Spotify Accounts list 1. :six: Nitro Codes ( https://link-to.net/41622/coddes4 ) 240x Nitro Codes list 1. :seven: Crunchyroll Accounts ( https://link-to.net/41622/crunch2 ). 67x Crunchyroll Accounts list 1. :eight: Nord Vpn Accounts ( https://link-to.net/41622/norde3 ) 200x nordVPN Accounts list 3. :nine: Origin Accounts ( https://up-to-down.net/41622/origin101 ) 74x Origin Accounts list 1.`;
    var words = string.split('\n');
     let random = words[Math.floor(Math.random()*words.length)];
       message.author.send("**Join Tabish Gen Official CM** https://discord.gg/zGTR2Ra");
      message.author.send({embed: {
         color: 3447003,
-        description: `**Spotify Premium Accounts** \n \n ${random}`
+        description: `**All Accounts** \n \n ${random}`
       }});
       message.channel.send({embed: {
         color: 3066993,
-        description: "Spotify Premium accounts sent on **DMs**! :white_check_mark:"
+        description: "Accounts sent on **DMs**! :white_check_mark:"
    }});
     };
     
