0bac7bd1ce7b230d26bb617269d5e510a2ba0351
tabishshah139 <54994056+tabishshah139@users.noreply.github.com>
2019-12-08 19:41:52 +0500

    Update server.js

--- server.js
+++ server.js
@@ -17,6 +17,36 @@ bot.user.setActivity(`${bot.guilds.size} servers ${bot.users.size} Users`);
 
 
 bot.on('message', message =>{
+    if (!message.guild) return;
+if (message.content === '+gen'){
+
+    if (usedCommandRecently4.has(message.author.id)){
+        message.channel.send('``Wait 2 mintue before generating again``')
+    } else{
+        usedCommandRecently4.add(message.author.id);
+        setTimeout(() =>{
+            usedCommandRecently4.delete(message.author.id);
+        }, 1000)
+
+    var string = `Note: We have added Little Advertisement | | You Can Get more than 50 Accounts by seeing advertisement 1 time | | :one: Mail Access ( https://link-to.net/41622/mailaccc4 ) 150x Mail Access Accounts list 1 | |  :two: Minecraft Accounts ( https://up-to-down.net/41622/spotifyaccounts ) 149x Minecraft Accounts list 1 | | :three: Hulu Accounts ( https://up-to-down.net/41622/hullus ) 1000x Hulu Accounts list | | :four: Psn Accounts ( https://up-to-down.net/41622/psnacounts ) 250x Psn Accounts list | | :five: Spotify Accounts ( https://up-to-down.net/41622/spotifyreal ) 200x Spotify Accounts list 1 | | :six: Nitro Codes ( https://link-to.net/41622/coddes4 ) 240x Nitro Codes list 1 | | :seven: Crunchyroll Accounts ( https://up-to-down.net/41622/crunchyrolls ). 67x Crunchyroll Accounts list 1 | | :eight: Nord Vpn Accounts ( https://link-to.net/41622/norde3 ) 200x nordVPN Accounts list 3| | :one::zero: Fortnite Accounts ( https://up-to-down.net/41622/fortniteaccounts ) 799x Fortnite Accounts list 1. | | :one::one: Steam Accounts ( https://up-to-down.net/41622/steamsaccounts ) 2319x Steam Accounts list 1. | | :one::two: Uplay Accounts ( https://link-to.net/41622/uply1 ) 500x Uplay Accounts list 1. | | :one::three: Pornhub Accounts ( https://up-to-down.net/41622/ponograhphy ) 120x Pornhub Accounts list 1. | | :one::five: HBO Now Premium Accounts ( https://direct-link.net/41622/hbonow1 ) 50x HBO Now Premium Accounts list 1. | | :one::six: Minty Codes ( https://direct-link.net/41622/mintydew1 ) 30x Minty Codes list 1. | | :one::seven: Disney Plus Premium ( https://direct-link.net/41622/disneyew1 ) Disney Plus Premium Accounts. | | :one::eight: Mega.nz Accounts ( https://direct-link.net/41622/meganzz ) Mega.nz Accounts. | | :one::nine: Paypal Balance Accounts ( https://link-to.net/41622/paypal231 ) Paypal Balance Accounts.`;
+   var words = string.split('\n');
+    let random = words[Math.floor(Math.random()*words.length)];
+      message.author.send("**Join Tabish Gen Official CM** https://discord.gg/X8ZGedf .Tutorial About Generating Accounts https://youtu.be/jBILyOwZDtY. You Can Invite Tabish Gen At Your Server type +invite.");
+     message.author.send({embed: {
+        color: 3447003,
+        description: `**All Accounts** \n \n ${random}`
+      }});
+      message.channel.send({embed: {
+        color: 3066993,
+        description: "Accounts sent on **DMs**! :white_check_mark:"
+   }});
+    };
+    
+};
+});
+
+
+bot.on('message', message =>{
 if (message.content == "!hulu" || message.content == "-hulu" || message.content == "!crunchyroll" || message.content == "-crunchyroll" || message.content == "!nordvpn" || message.content == "-nordvpn" || message.content == "!pornhub" || message.content == "-pornhub" || message.content == "!minecraft" || message.content == "-minecraft" || message.content == "!wwe" || message.content == "-wwe" || message.content == "!mailaccess" || message.content == "-mailaccess" || message.content == "!origin" || message.content == "-origin" || message.content == "!hbo" || message.content == "-hbo" || message.content == "!pubg" || message.content == "-pubg" || message.content == "!minty" || message.content == "-minty" || message.content == "!ebay" || message.content == "-ebay" || message.content == "!nitro" || message.content == "-nitro" || message.content == "+stock" || message.content == "-stock" || message.content == "!stock" || message.content == ">stock" || message.content == "+stocks" || message.content == "-stocks" || message.content == "!stocks" || message.content == ">stocks" || message.content == "!invite" || message.content == "-invite" || message.content == "!invite" || message.content == "!help" || message.content == "-help" || message.content == "-gen" || message.content == "!gen" || message.content == "!gen fortnite" || message.content == "!fortnite" || message.content == "-fortnite" || message.content == "!steam" || message.content == "!spotify" || message.content == "-spotify" || message.content == "-steam" || message.content == "!uplay" || message.content == "-uplay" || message.content == "!netflix" || message.content == "-netflix" || message.content == "+spotify" || message.content == "+steam" || message.content == "+hulu" || message.content == "+crunchyroll" || message.content == "+minecraft" || message.content == "+nordvpn" || message.content == "+minty" || message.content == "+ebay" || message.content == "+origin" || message.content == "+fortnite" || message.content == "+netflix" || message.content == "+uplay" || message.content == ">spotify" || message.content == ">uplay" || message.content == ">fortnite" || message.content == ">pornhub" || message.content == ">nordvpn" || message.content == ">minecraft" || message.content == ">hulu" || message.content == ">crunchyroll" || message.content == ">steam" || message.content == ">mailaccess" || message.content == ">netflix") { 
     message.channel.send("```Sorry There is Just One Command (+gen) to generate all account which is available at stocks type +gen```")
     message.author.send("```Sorry There is Just One Command (+gen) to generate all account which is available at stocks type +gen```");
