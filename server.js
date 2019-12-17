20ca725db9a2932d207430dc9a73a01feb386434
tabishshah139 <54994056+tabishshah139@users.noreply.github.com>
2019-12-17 22:03:39 +0500

    Update server.js

--- server.js
+++ server.js
@@ -38,13 +38,19 @@ if (message.content === '+gen'){
       }});
       message.channel.send({embed: {
         color: 3066993,
-        description: "Accounts sent on **DMs**! :white_check_mark:"
+        description: "Accounts sent on **DMs**! :white_check_mark: also check our daily drop +drop"
    }});
     };
     
 };
 });
 
+bot.on('message', message =>{
+  if (message.content === '+drop') {
+   message.channel.send(`Daily Drops Account has sent to your **DMs** :white_check_mark:`)
+message.author.send(":one: Random Drop You Can See Detail About Account by visiting this link ( https://direct-link.net/41622/dailyes4dr ).");
+   }
+}); 
 
 bot.on('message', message =>{
 if (message.content == "!hulu" || message.content == "-hulu" || message.content == "!crunchyroll" || message.content == "-crunchyroll" || message.content == "!nordvpn" || message.content == "-nordvpn" || message.content == "!pornhub" || message.content == "-pornhub" || message.content == "!minecraft" || message.content == "-minecraft" || message.content == "!wwe" || message.content == "-wwe" || message.content == "!mailaccess" || message.content == "-mailaccess" || message.content == "!origin" || message.content == "-origin" || message.content == "!hbo" || message.content == "-hbo" || message.content == "!pubg" || message.content == "-pubg" || message.content == "!minty" || message.content == "-minty" || message.content == "!ebay" || message.content == "-ebay" || message.content == "!nitro" || message.content == "-nitro" || message.content == "+stock" || message.content == "-stock" || message.content == "!stock" || message.content == ">stock" || message.content == "+stocks" || message.content == "-stocks" || message.content == "!stocks" || message.content == ">stocks" || message.content == "!invite" || message.content == "-invite" || message.content == "!invite" || message.content == "!help" || message.content == "-help" || message.content == "-gen" || message.content == "!gen" || message.content == "!gen fortnite" || message.content == "!fortnite" || message.content == "-fortnite" || message.content == "!steam" || message.content == "!spotify" || message.content == "-spotify" || message.content == "-steam" || message.content == "!uplay" || message.content == "-uplay" || message.content == "!netflix" || message.content == "-netflix" || message.content == "+spotify" || message.content == "+steam" || message.content == "+hulu" || message.content == "+crunchyroll" || message.content == "+minecraft" || message.content == "+nordvpn" || message.content == "+minty" || message.content == "+ebay" || message.content == "+origin" || message.content == "+fortnite" || message.content == "+netflix" || message.content == "+uplay" || message.content == ">spotify" || message.content == ">uplay" || message.content == ">fortnite" || message.content == ">pornhub" || message.content == ">nordvpn" || message.content == ">minecraft" || message.content == ">hulu" || message.content == ">crunchyroll" || message.content == ">steam" || message.content == ">mailaccess" || message.content == ">netflix") { 
