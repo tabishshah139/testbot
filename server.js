5e35f04dad476c531a9b7243f0a5ada62815e7ef
tabishshah139 <54994056+tabishshah139@users.noreply.github.com>
2019-12-18 21:30:10 +0500

    Update server.js

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ if (message.content === '+gen'){
 bot.on('message', message =>{
   if (message.content === '+drop') {
    message.channel.send(`Daily Drops Account has sent to your **DMs** :white_check_mark:`)
-message.author.send(":one: Random Drop You Can See Detail About Account by visiting this link ( https://direct-link.net/41622/dailyes4dr ).");
+message.author.send(":one: Random Drop You Can See Detail About Account by visiting this link 18 december ( https://link-to.net/41622/dailydrop ).");
    }
 }); 
 
