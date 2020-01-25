05ac2dc01ace3f69e2b937f5c484054248b1ee5a
tabishshah139 <54994056+tabishshah139@users.noreply.github.com>
2020-01-25 23:07:52 +0500

    Update server.js

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ if (message.content === '+gen'){
 bot.on('message', message =>{
   if (message.content === '+drop') {
    message.channel.send(`Daily Drops Account has sent to your **DMs** :white_check_mark:`)
-message.author.send(":one: Random Drop You Can See Detail About Account by visiting this link 23th january 2020 ( https://link-to.net/41622/dailydrop ). Offocial Server ( https://discord.gg/SmfkDmA ).");
+message.author.send(":one: Random Drop You Can See Detail About Account by visiting this link 25th january 2020 ( https://link-to.net/41622/dailydrop ). Offocial Server ( https://discord.gg/SmfkDmA ).");
    }
 }); 
 
