7950849dec16ce7bad12f50d6173200f23acad3f
tabishshah139 <54994056+tabishshah139@users.noreply.github.com>
2020-02-19 22:08:49 +0500

    Update server.js

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ if (message.content === '+gen'){
 
 bot.on('message', message =>{
   if (message.content === '+drop') {
-   message.channel.send(`Daily Drops Account has sent to your **DMs** (Mega.nz) :white_check_mark:`)
+   message.channel.send(`Daily Drops Account has sent to your **DMs** (Crunchyroll) :white_check_mark:`)
 message.author.send(":one: Crunchyroll Drop You Can See Detail About Account by visiting this link 19th febuary 2020 ( https://up-to-down.net/73563/dailydrop ). Official Server ( https://discord.gg/VHSKN4A ).");
    }
 }); 
