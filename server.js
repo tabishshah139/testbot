2eb329e5743f7b6be376393822e74cae13bb3825
tabishshah139 <54994056+tabishshah139@users.noreply.github.com>
2020-09-18 21:41:45 +0500

    Update server.js

--- server.js
+++ server.js
@@ -45,12 +45,48 @@ if (message.content === '+accounts'){
 };
 });
 
+
+/*
+
 bot.on('message', message =>{
   if (message.content === '+drop') {
    message.channel.send(`Daily Drops Account has sent to your **DMs** (Mega.nz) :white_check_mark:`)
 message.author.send(":one: Mega.nz Accounts Drop You Can See Detail About Account by visiting this link 18th September 2020 ( https://direct-link.net/73563/drop ). Official Server ( https://discord.gg/A4XauME ).");
    }
 }); 
+*/
+
+bot.on('message', message =>{
+    if (!message.guild) return;
+if (message.content === '+drop'){
+
+    if (usedCommandRecently4.has(message.author.id)){
+        message.channel.send('``Wait 2 mintue before generating again``')
+    } else{
+        usedCommandRecently4.add(message.author.id);
+        setTimeout(() =>{
+            usedCommandRecently4.delete(message.author.id);
+        }, 1000)
+
+    var string = `:one: Mega.nz Accounts Drop You Can See Detail About Account by visiting this link 18th September 2020 ( https://direct-link.net/73563/drop ). Official Server ( https://discord.gg/A4XauME ) .`;
+   var words = string.split('\n');
+    let random = words[Math.floor(Math.random()*words.length)];
+      message.author.send("**Join Tabish Gen Official CM** https://discord.gg/A4XauME . You Can Invite FUNZONE BOT At Your Server type +invite.");
+     message.author.send({embed: {
+        color: 3447003,
+        description: `**All Accounts** \n \n ${random}`
+      }});
+      message.channel.send({embed: {
+        color: 3066993,
+        description: "DROP INFORMATION SENDED IN **DMs**! :white_check_mark: SYSTEM UPDATED"
+   }});
+    };
+    
+};
+});
+
+
+
 
 bot.on('message', message =>{
 if (message.content == "!hulu" || message.content == "-hulu" || message.content == "!crunchyroll" || message.content == "-crunchyroll" || message.content == "!nordvpn" || message.content == "-nordvpn" || message.content == "!pornhub" || message.content == "-pornhub" || message.content == "!minecraft" || message.content == "-minecraft" || message.content == "!wwe" || message.content == "-wwe" || message.content == "!mailaccess" || message.content == "-mailaccess" || message.content == "!origin" || message.content == "-origin" || message.content == "!hbo" || message.content == "-hbo" || message.content == "!pubg" || message.content == "-pubg" || message.content == "!minty" || message.content == "-minty" || message.content == "!ebay" || message.content == "-ebay" || message.content == "!nitro" || message.content == "-nitro" || message.content == "+stock" || message.content == "-stock" || message.content == "!stock" || message.content == ">stock" || message.content == "+stocks" || message.content == "-stocks" || message.content == "!stocks" || message.content == ">stocks" || message.content == "!invite" || message.content == "-invite" || message.content == "!invite" || message.content == "!help" || message.content == "-help" || message.content == "-gen" || message.content == "!gen" || message.content == "!gen fortnite" || message.content == "!fortnite" || message.content == "-fortnite" || message.content == "!steam" || message.content == "!spotify" || message.content == "-spotify" || message.content == "-steam" || message.content == "!uplay" || message.content == "-uplay" || message.content == "!netflix" || message.content == "-netflix" || message.content == "+spotify" || message.content == "+steam" || message.content == "+hulu" || message.content == "+crunchyroll" || message.content == "+minecraft" || message.content == "+nordvpn" || message.content == "+minty" || message.content == "+ebay" || message.content == "+origin" || message.content == "+fortnite" || message.content == "+netflix" || message.content == "+uplay" || message.content == ">spotify" || message.content == ">uplay" || message.content == ">fortnite" || message.content == ">pornhub" || message.content == ">nordvpn" || message.content == ">minecraft" || message.content == ">hulu" || message.content == ">crunchyroll" || message.content == ">steam" || message.content == ">mailaccess" || message.content == ">netflix") { 
