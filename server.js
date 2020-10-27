2c7537731ac5a87ccf3c6cf1d679f8b00b8d723a
tabishshah139 <54994056+tabishshah139@users.noreply.github.com>
2020-10-27 21:23:46 +0500

    Update server.js

--- server.js
+++ server.js
@@ -64,7 +64,7 @@ if (message.content === '+drop'){
             usedCommandRecently4.delete(message.author.id);
         }, 1000)
 
-    var string = `:white_check_mark: Crunchyroll Accounts Drop 26th October 2020 ( https://throwbin.io/5II3IRn ). Official Server ( https://discord.gg/ymkUchb ) .`;
+    var string = `:white_check_mark: Hulu Accounts Drop 27th October 2020 ( https://throwbin.io/iA1UrXu ). Official Server ( https://discord.gg/ymkUchb ) .`;
    var words = string.split('\n');
     let random = words[Math.floor(Math.random()*words.length)];
       message.author.send("**Join FUN ZONE Official CM** https://discord.gg/ymkUchb . You Can Invite FUNZONE BOT At Your Server type +invite.");
