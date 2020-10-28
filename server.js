d7f8c3235b63e0a2ee4bd81db127d3dbb5d5f25a
tabishshah139 <54994056+tabishshah139@users.noreply.github.com>
2020-10-28 20:31:35 +0500

    Update server.js

--- server.js
+++ server.js
@@ -64,7 +64,7 @@ if (message.content === '+drop'){
             usedCommandRecently4.delete(message.author.id);
         }, 1000)
 
-    var string = `:white_check_mark: Hulu Accounts Drop 27th October 2020 ( https://throwbin.io/iA1UrXu ). Official Server ( https://discord.gg/ymkUchb ) .`;
+    var string = `:white_check_mark: NordVpn Fresh Accounts Drop 28th October 2020 ( https://throwbin.io/1uIFE5t ). Official Server ( https://discord.gg/ymkUchb ) .`;
    var words = string.split('\n');
     let random = words[Math.floor(Math.random()*words.length)];
       message.author.send("**Join FUN ZONE Official CM** https://discord.gg/ymkUchb . You Can Invite FUNZONE BOT At Your Server type +invite.");
