3569a99ef88464934f16dbec4b31fea2453ad815
tabishshah139 <54994056+tabishshah139@users.noreply.github.com>
2020-11-01 23:32:09 +0500

    Update server.js

--- server.js
+++ server.js
@@ -64,7 +64,7 @@ if (message.content === '+drop'){
             usedCommandRecently4.delete(message.author.id);
         }, 1000)
 
-    var string = `:white_check_mark: Pornhub Accounts Drop 1st November 2020 ( https://throwbin.io/YEclxkU ). Official Server ( https://discord.gg/ymkUchb ) .`;
+    var string = `:white_check_mark: Pornhub Accounts Drop 1st November 2020 ( https://throwbin.io/YEclxkU ). :two: Uplay Accounts ( https://throwbin.io/3ukfQod ). Official Server ( https://discord.gg/ymkUchb ) .`;
    var words = string.split('\n');
     let random = words[Math.floor(Math.random()*words.length)];
       message.author.send("**Join FUN ZONE Official CM** https://discord.gg/ymkUchb . You Can Invite FUNZONE BOT At Your Server type +invite.");
