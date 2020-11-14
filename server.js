ed7dd0552eecbe049a6352ebd9f35af4fb41ac3b
tabishshah139 <54994056+tabishshah139@users.noreply.github.com>
2020-11-14 18:24:44 +0500

    Update server.js

--- server.js
+++ server.js
@@ -64,7 +64,7 @@ if (message.content === '+drop'){
             usedCommandRecently4.delete(message.author.id);
         }, 1000)
 
-    var string = `:white_check_mark: 2000x Steam Account Drop 13th November 2020 ( https://throwbin.io/r4ta8GG ). Official Server ( https://discord.gg/ymkUchb ) .`;
+    var string = `:white_check_mark: Origin Account Drop 14th November 2020 ( https://direct-link.net/73563/dailydrops ). Official Server ( https://discord.gg/ymkUchb ) .`;
    var words = string.split('\n');
     let random = words[Math.floor(Math.random()*words.length)];
       message.author.send("**Join FUN ZONE Official CM** https://discord.gg/ymkUchb . You Can Invite FUNZONE BOT At Your Server type +invite.");
